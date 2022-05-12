b34a3280fe4945dfbfffa767bf0f0b6ea65c7a7a
Jerónimo Robalino <70788587+jeronimo-art@users.noreply.github.com>
2022-05-12 10:09:02 -0500

    Add files via upload

---
+++ precache-manifest.f3c47cf8a3d8c2c114df535b59a52ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "764fd0dee0f1ca3429ab",
+    "url": "/static/css/main.3e5653d6.chunk.css"
+  },
+  {
+    "revision": "764fd0dee0f1ca3429ab",
+    "url": "/static/js/main.764fd0de.chunk.js"
+  },
+  {
+    "revision": "c2638392ebdcb7889697",
+    "url": "/static/js/1.c2638392.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c983f6b8a77291ee6b13a4b4939c368a",
+    "url": "/static/media/todo-svg.c983f6b8.svg"
+  },
+  {
+    "revision": "77f3da3fe5922e0bcb4a9f5881f32ea7",
+    "url": "/index.html"
+  }
+];
